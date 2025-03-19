3fe326cd259cc5a4ca44af2d6098064c8513f862
parth <psomani16k@gmail.com>
2025-03-19 11:52:02 +0530

    borders in neovim lsp

--- nvim/lua/parth/plugins/lsp.lua
+++ nvim/lua/parth/plugins/lsp.lua
@@ -17,9 +17,25 @@ return {
 		require("mason-lspconfig").setup {
 			ensure_installed = { "markdown_oxide", "clangd", "lua_ls", "rust_analyzer" },
 		}
-		require("lspconfig").lua_ls.setup {}
-		require("lspconfig").rust_analyzer.setup {}
-		require("lspconfig").clangd.setup {}
-		require("lspconfig").markdown_oxide.setup {}
+		local border = {
+			{ "🭽", "FloatBorder" },
+			{ "▔", "FloatBorder" },
+			{ "🭾", "FloatBorder" },
+			{ "▕", "FloatBorder" },
+			{ "🭿", "FloatBorder" },
+			{ "▁", "FloatBorder" },
+			{ "🭼", "FloatBorder" },
+			{ "▏", "FloatBorder" },
+		}
+
+		-- LSP settings (for overriding per client)
+		local handlers = {
+			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+		}
+		require("lspconfig").lua_ls.setup { handlers = handlers }
+		require("lspconfig").rust_analyzer.setup { handlers = handlers }
+		require("lspconfig").clangd.setup { handlers = handlers }
+		require("lspconfig").markdown_oxide.setup { handlers = handlers }
 	end
 }
