ff343a048f4db25e1959fb4d05adcc9497e1c8d7
parth <psomani16k@gmail.com>
2025-03-31 15:40:27 +0530

    checking out lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,2 @@
-require("parth.core")
-require("parth.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/parth/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "npm install", -- needs nodejs and npm installed on the system
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
-}
