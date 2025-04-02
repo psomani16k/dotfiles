5c92028a40734d0be3f7fef47f3dcb9b3f38606c
parth <psomani16k@gmail.com>
2025-04-02 08:38:55 +0530

    .

---
+++ .setup_fedora.sh
@@ -0,0 +1,3 @@
+sudo dnf install -y https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203_110809_5046fc22-1.fedora39.x86_64.rpm
+sudo dnf update
+sudo dnf install neovim fish curl wezterm ghostty
