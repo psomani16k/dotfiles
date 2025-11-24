09376b876e67aa4199cb4d324f5bb26c746b78d4
parth <psomani16k@gmail.com>
2025-11-24 10:27:47 +0530

    niri

--- nvim/lua/parth/core/keymaps.lua
+++ nvim/lua/parth/core/keymaps.lua
@@ -56,8 +56,14 @@ keymap.set("n", "<leader>.", function() vim.lsp.buf.code_action() end, { desc = 
 keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, {})
 keymap.set("n", "<leader>ln", function() vim.lsp.buf.rename() end, { desc = "Rename" })
 keymap.set("n", "<leader>li", function() vim.lsp.buf.implementation() end, { desc = "Go To implementation" })
-keymap.set("n", "<leader>ii", function() require("conform").format({ async = true, lsp_fallback = true }) end,
-  { desc = "Format using Formatter or LSP" })
+keymap.set("n", "<leader>ii", function()
+  local didFormat = require("conform").format({ async = false, lsp_fallback = true })
+  print(tostring(didFormat))
+  if not didFormat then
+    -- Keystrokes to simulate: Ctrl + =
+    vim.cmd('normal!ggGV')
+  end
+end, { desc = "Format using Formatter or LSP" })
 keymap.set("n", "<leader>ll", function() vim.lsp.buf.signature_help({ border = "double" }) end,
   { desc = "Signature Help" })
 keymap.set("n", "<leader>er", function() vim.diagnostic.open_float({ border = "double" }) end,
