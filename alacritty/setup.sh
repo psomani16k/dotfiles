d9fc6557072bec9349112feeb04190cb90109a0c
parth_somani <parth.s@even.in>
2025-11-26 00:12:37 +0530

    setup

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Install alacritty 
-echo -e "\033[1;32m|=== INSTALLING ALACRITTY ===|\033[0m"
+echo -e "\033[1;32m|=== INSTALLING AND SETTING UP ALACRITTY ===|\033[0m"
 sudo dnf install -y alacritty
 echo ""
+
+# Setting up CaskaydiaCove fonts
+mkdir ~/.fonts
+cp ./CascadiaCode/* ~/.fonts
+sudo fc-cache -fv
