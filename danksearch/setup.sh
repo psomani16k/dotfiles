8db1e67c778a3364ae7d9aef91e75d4d9da3a6ce
parth_somani <parth.s@even.in>
2025-11-26 00:05:58 +0530

    setup

--- danksearch/setup.sh
+++ danksearch/setup.sh
@@ -2,10 +2,7 @@
 
 # Installing dsearch
 echo -e "\033[1;32m|=== INSTALLING AND SETTINGUP DSEARCH ===|\033[0m"
-sudo dnf copr enable avengemedia/danklinux
-sudo dnf install dsearch
-dms greeter enable
-dms greeter sync
-sudo systemctl disable gdm lightdm sddm
-sudo systemctl enable greetd
+sudo dnf copr enable avengemedia/danklinux -y
+sudo dnf install dsearch -y
+dsearch index generate
 echo ""
