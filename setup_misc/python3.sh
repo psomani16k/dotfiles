6b098253db32c3ba829f5acd4d5d396bcaff8558
parth_somani <parth.s@even.in>
2025-11-25 20:46:31 +0530

    setup

--- setup_misc/python3.sh
+++ setup_misc/python3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Installing python 3
-echo -e "\033[1;32m|=== INSTALLING CARGO BINSTALL ===|\033[0m"
-cargo inatall cargo-binstall
+echo -e "\033[1;32m|=== INSTALLING PYTHON ===|\033[0m"
+sudo dnf install python3
