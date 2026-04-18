7609d1ee8d775016b5c2961b78caafc4f09ae21b
parth <psomani16k@gmail.com>
2026-04-18 18:39:43 +0530

    update

--- fish/config.fish
+++ fish/config.fish
@@ -34,10 +34,10 @@ source ~/.ssh/api_keys.fish
 
 # android build caching
 set -x USE_CCACHE 1
-set -x CCACHE_EXEC "/usr/bin/ccache"
+set -x CCACHE_EXEC /usr/bin/ccache
 
 # using a superior editor
-set -x EDITOR "/usr/bin/nvim"
+set -x EDITOR /usr/bin/nvim
 
 # go and bazel related stuff
 set -x USE_BAZEL_VERSION 6.4.0
@@ -48,16 +48,16 @@ set -x JAVA_HOME /usr/lib/jvm/jre-25-openjdk
 #     # Maybe set ZELLIJ_AUTO_ATTACH / ZELLIJ_AUTO_EXIT
 #     eval (zellij setup --generate-auto-start fish | string collect)
 # end
-if not set -q ZELLIJ                                                                                                                                                           
-    if test "$ZELLIJ_AUTO_ATTACH" = "true"                                                                                                                                     
-        zellij attach -c                                                                                                                                                       
-    else                                                                                                                                                                       
-        zellij                                                                                                                                                                 
-    end                                                                                                                                                                        
-                                                                                                                                                                               
-    if test "$ZELLIJ_AUTO_EXIT" = "true"                                                                                                                                       
-        kill $fish_pid                                                                                                                                                         
-    end                                                                                                                                                                        
+if not set -q ZELLIJ
+    if test "$ZELLIJ_AUTO_ATTACH" = true
+        zellij attach -c
+    else
+        zellij
+    end
+
+    if test "$ZELLIJ_AUTO_EXIT" = true
+        kill $fish_pid
+    end
 end
 
 set -x GITLAB_HOME /srv/gitlab
