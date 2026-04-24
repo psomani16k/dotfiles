0a14d41240be141730eb00e9f203cd6ec557e9a4
parth <parth.s>
2026-04-24 20:45:32 +0530

    update

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ alias rmrf="rm -rf"
 alias qq="exit"
 alias obsidian="flatpak run md.obsidian.Obsidian"
 alias clear="echo 'Please use CTRL+L'"
-alias laude="claude"
+alias laude="/usr/local/bin/claude"
 alias headcommit="git log --oneline | head -n 1"
 
 # paths
